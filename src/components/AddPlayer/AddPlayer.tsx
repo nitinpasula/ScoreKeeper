4f5fafe9620cefb8e3a167aa5496fa7ceec12ddd
npasula <npasula@ptc.com>
2020-07-17 14:01:42 -0500

    Updates to add player 1. capitalize each word in a name 2. not add player with empty name and provide indicator for missing input

--- src/components/AddPlayer/AddPlayer.tsx
+++ src/components/AddPlayer/AddPlayer.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Player from "../../model/Player";
 import { connect } from "react-redux";
 import { addPlayer } from "../../redux/actions";
-
+import "./AddPlayer.css";
 const AddPlayer: React.FC<any> = (props) => {
+  const [valid, setValid] = useState(true);
   const capitalizeName = (name: string): string => {
-    return name
-      .split(" ")
-      .map((w) => w[0].toUpperCase() + w.slice(1))
-      .join(" ");
+    return name.trim()
+      ? name
+          .split(" ")
+          .map((w) => w[0].toUpperCase() + w.slice(1))
+          .join(" ")
+      : name.trim();
   };
 
   const handlePlayerNameChange = (
@@ -16,6 +19,10 @@ const AddPlayer: React.FC<any> = (props) => {
   ) => {
     if (event.keyCode === 13) {
       const playerName = capitalizeName(event.currentTarget.value);
+      if (!playerName) {
+        setValid(false);
+        return;
+      }
       const newPlayer: Player = {
         name: playerName,
         scores: [],
@@ -23,6 +30,7 @@ const AddPlayer: React.FC<any> = (props) => {
       };
       props.doAddPlayer(newPlayer);
       event.currentTarget.value = "";
+      setValid(true);
     }
   };
   const handleAddPlayer = () => {
@@ -30,6 +38,10 @@ const AddPlayer: React.FC<any> = (props) => {
       "addplayer"
     ) as HTMLInputElement;
     const playerName = capitalizeName(inputElement.value);
+    if (!playerName) {
+      setValid(false);
+      return;
+    }
     const newPlayer: Player = {
       name: playerName,
       scores: [],
@@ -37,11 +49,13 @@ const AddPlayer: React.FC<any> = (props) => {
     };
     props.doAddPlayer(newPlayer);
     inputElement.value = "";
+    setValid(true);
   };
   return (
     <label htmlFor="addPlayer">
       Player Name:
       <input
+        className={valid ? undefined : "empty-player"}
         id="addplayer"
         type="text"
         placeholder="Enter player name"
